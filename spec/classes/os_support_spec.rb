900e7c93fd565b3a6d8561348e3e4a0a1cd1dcbe
Jordi Prats <jprats@systemadmin.es>
2016-01-25 16:09:06 +0100

    afegit rspec centos 6

--- spec/classes/os_support_spec.rb
+++ spec/classes/os_support_spec.rb
@@ -1,6 +1,25 @@
 require 'spec_helper'
 describe 'logstash' do
 
+  context 'CentOS 6' do
+
+    let (:params) { { 'version' => '1.5' } }
+
+    let :facts do
+    {
+      :osfamily => 'RedHat',
+      :operatingsystem => 'CentOS',
+      :operatingsystemrelease => '6.3',
+    }
+    end
+
+    it { should contain_class('logstash') }
+
+    it { should contain_package('logstash')}
+
+    it { should contain_service('logstash')}
+  end
+
   context 'CentOS 7' do
 
     let (:params) { { 'version' => '1.5' } }
