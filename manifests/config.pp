b5e9effb1f161f0650bc9e6952acbeb00245e51e
Jordi Prats <jordi.prats@gmail.com>
2018-08-20 17:13:00 +0200

    bugfix init service

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,7 @@
 class logstash::config inherits logstash {
-
+  exec { 'postinstall action':
+    command => $logstash::params::postinstall_action,
+    creates => $logstash::params::postinstall_creates,
+    path    => '/usr/sbin:/usr/bin:/sbin :/bin'
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -13,6 +13,8 @@ class logstash::params {
         {
           $package_url='https://artifacts.elastic.co/downloads/logstash/logstash-6.3.1.rpm'
           $package_provider = 'rpm'
+          $postinstall_action = '/usr/share/logstash/bin/system-install /etc/logstash/startup.options systemd'
+          $postinstall_creates = '/etc/systemd/system/logstash.service'
         }
         default: { fail('Unsupported RHEL/CentOS version!')  }
       }
