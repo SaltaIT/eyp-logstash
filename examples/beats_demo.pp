165489c6a638dcbd9cc7da43cae74a94fb2bef12
Jordi Prats <jordi.prats@gmail.com>
2018-10-25 12:18:43 +0200

    conditional output

--- examples/beats_demo.pp
+++ examples/beats_demo.pp
@@ -2,4 +2,26 @@ class { 'logstash': }
 
 logstash::input::beats { 'beats': }
 
-logstash::output::elasticsearch { 'elasticsearch': }
+logstash::output::elasticsearch { 'elasticsearch1':
+  order => '00',
+  hosts => [ '1.1.1.1' ],
+  index => 'auditbeat-%{+YYYY.MM.dd}',
+  conditional_output_field => 'auditbeat',
+  conditional_output_value => true,
+}
+
+logstash::output::elasticsearch { 'elasticsearch2':
+  order => '01',
+  hosts => [ '2.2.2.2' ],
+  index => 'filebeat-%{+YYYY.MM.dd}',
+  conditional_output_statement => 'else if'
+  conditional_output_field => 'filebeat',
+  conditional_output_value => true,
+}
+
+logstash::output::elasticsearch { 'elasticsearch2':
+  order => '03',
+  hosts => [ '2.2.2.2' ],
+  index => 'other-%{+YYYY.MM.dd}',
+  conditional_output_statement => 'else'
+}
--- manifests/output/elasticsearch.pp
+++ manifests/output/elasticsearch.pp
@@ -1,8 +1,12 @@
 define logstash::output::elasticsearch(
-                                        $id    = $name,
-                                        $order = '42',
-                                        $hosts = [ '127.0.0.1' ],
-                                        $index = undef,
+                                        $id                           = $name,
+                                        $order                        = '00',
+                                        $hosts                        = [ '127.0.0.1' ],
+                                        $index                        = undef,
+                                        $conditional_output_statement = 'if',
+                                        $conditional_output_field     = undef,
+                                        $conditional_output_value     = '',
+                                        $conditional_output_condition = '==',
                                       ) {
   if(!defined(Concat['/etc/logstash/conf.d/99_output.conf']))
   {
@@ -16,20 +20,20 @@ define logstash::output::elasticsearch(
 
     concat::fragment{ 'logstash 99_output.conf header':
       target  => '/etc/logstash/conf.d/99_output.conf',
-      order   => '00',
+      order   => 'a00',
       content => "output {\n",
     }
 
     concat::fragment{ 'logstash 99_output.conf end':
       target  => '/etc/logstash/conf.d/99_output.conf',
-      order   => '99',
+      order   => 'z99',
       content => "\n}\n",
     }
   }
 
   concat::fragment{ "logstash 99_output.conf elasticsearch ${id}":
     target  => '/etc/logstash/conf.d/99_output.conf',
-    order   => "42-${order}",
+    order   => "b42-${order}",
     content => template("${module_name}/output/elasticsearch.erb"),
   }
 
